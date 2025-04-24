3fbb435cafd4f8645f626e6fc74a294dd5107af2
Ruby Bui <ruby@coderpush.com>
2025-04-24 05:35:41 -0500

    let rabbit test this crud

--- mind_matter_api/api/__init__.py
+++ mind_matter_api/api/__init__.py
@@ -1,8 +1,13 @@
 # mind_matter_api/api/__init__.py
-
+# users
 from mind_matter_api.api.users import init_user_routes
 from mind_matter_api.repositories.users import UserRepository
 from mind_matter_api.services.users import UserService
+
+# surveys
+from mind_matter_api.api.surveys import init_survey_routes
+from mind_matter_api.repositories.surveys import SurveyRepository
+from mind_matter_api.services.surveys import SurveyService
 
 def register_routes(app):
     # --- wire up your services ---
@@ -11,10 +16,6 @@ def register_routes(app):
     # --- mount your route init functions ---
     init_user_routes(app)
 
-    # in future you can add:
-    # from mind_matter_api.api.surveys import init_survey_routes
-    # from mind_matter_api.repositories.surveys import SurveyRepository
-    # from mind_matter_api.services.surveys import SurveyService
-    #
-    # app.survey_service = SurveyService(SurveyRepository())
-    # init_survey_routes(app)
+
+    app.survey_service = SurveyService(SurveyRepository())
+    init_survey_routes(app)
